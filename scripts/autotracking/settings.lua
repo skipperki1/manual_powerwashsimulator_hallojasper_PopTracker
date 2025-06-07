b0d6f70356cc771cc60e4b8e344dc9f154fe6445
skipperki1 <skipperki1@gmail.com>
2025-06-06 22:15:58 -0400

    Item and Setting mapping

---
+++ scripts/autotracking/settings.lua
@@ -0,0 +1,143 @@
+function YesTemple()
+	return (1 - Tracker:ProviderCountForCode("YesTemple") > 0)
+end
+
+function YesForestCottage()
+	return (1 - Tracker:ProviderCountForCode("YesForestCottage") > 0)
+end
+
+function YesFortuneTellersWagon()
+	return (1 - Tracker:ProviderCountForCode("YesFortuneTellersWagon") > 0)
+end
+
+function YesFireTruck()
+	return (1 - Tracker:ProviderCountForCode("YesFireTruck") > 0)
+end
+
+function YesShoeHouse()
+	return (1 - Tracker:ProviderCountForCode("YesShoeHouse") > 0)
+end
+
+function YesGrandpaMillersCar()
+	return (1 - Tracker:ProviderCountForCode("YesGrandpaMillersCar") > 0)
+end
+
+function YesPrivateJet()
+	return (1 - Tracker:ProviderCountForCode("YesPrivateJet") > 0)
+end
+
+function YesAncientStatue()
+	return (1 - Tracker:ProviderCountForCode("YesAncientStatue") > 0)
+end
+
+function YesSkatepark()
+	return (1 - Tracker:ProviderCountForCode("YesSkatepark") > 0)
+end
+
+function YesBackGarden()
+	return (1 - Tracker:ProviderCountForCode("YesBackGarden") > 0)
+end
+
+function YesFerrisWheel()
+	return (1 - Tracker:ProviderCountForCode("YesFerrisWheel") > 0)
+end
+
+function YesTreeHouse()
+	return (1 - Tracker:ProviderCountForCode("YesTreeHouse") > 0)
+end
+
+function YesBungalow()
+	return (1 - Tracker:ProviderCountForCode("YesBungalow") > 0)
+end
+
+function YesDrill()
+	return (1 - Tracker:ProviderCountForCode("YesDrill") > 0)
+end
+
+function YesVintageCar()
+	return (1 - Tracker:ProviderCountForCode("YesVintageCar") > 0)
+end
+
+function YesFireHelicopter()
+	return (1 - Tracker:ProviderCountForCode("YesFireHelicopter") > 0)
+end
+
+function YesWashroom()
+	return (1 - Tracker:ProviderCountForCode("YesWashroom") > 0)
+end
+
+function YesSubwayPlatform()
+	return (1 - Tracker:ProviderCountForCode("YesSubwayPlatform") > 0)
+end
+
+function YesVan()
+	return (1 - Tracker:ProviderCountForCode("YesVan") > 0)
+end
+
+function YesGolfCart()
+	return (1 - Tracker:ProviderCountForCode("YesGolfCart") > 0)
+end
+
+function YesFishingBoat()
+	return (1 - Tracker:ProviderCountForCode("YesFishingBoat") > 0)
+end
+
+function YesSUV()
+	return (1 - Tracker:ProviderCountForCode("YesSUV") > 0)
+end
+
+function YesRecreationVehicle()
+	return (1 - Tracker:ProviderCountForCode("YesRecreationVehicle") > 0)
+end
+
+function YesFrolicBoat()
+	return (1 - Tracker:ProviderCountForCode("YesFrolicBoat") > 0)
+end
+
+function YesMayorsMansion()
+	return (1 - Tracker:ProviderCountForCode("YesMayorsMansion") > 0)
+end
+
+function YesStuntPlane()
+	return (1 - Tracker:ProviderCountForCode("YesStuntPlane") > 0)
+end
+
+function YesPennyFarthing()
+	return (1 - Tracker:ProviderCountForCode("YesPennyFarthing") > 0)
+end
+
+function YesPlayground()
+	return (1 - Tracker:ProviderCountForCode("YesPlayground") > 0)
+end
+
+function YesMonsterTruck()
+	return (1 - Tracker:ProviderCountForCode("YesMonsterTruck") > 0)
+end
+
+function YesLostCityPalace()
+	return (1 - Tracker:ProviderCountForCode("YesLostCityPalace") > 0)
+end
+
+function YesDetachedHouse()
+	return (1 - Tracker:ProviderCountForCode("YesDetachedHouse") > 0)
+end
+
+function YesAncientMonument()
+	return (1 - Tracker:ProviderCountForCode("YesAncientMonument") > 0)
+end
+
+function YesRecreationVehicleAgain()
+	return (1 - Tracker:ProviderCountForCode("YesRecreationVehicleAgain") > 0)
+end
+
+function YesMotorbikeandSidecar()
+	return (1 - Tracker:ProviderCountForCode("YesMotorbikeandSidecar") > 0)
+end
+
+function YesDirtBike()
+	return (1 - Tracker:ProviderCountForCode("YesDirtBike") > 0)
+end
+
+function YesPartsMode()
+	return (1 - Tracker:ProviderCountForCode("PartsMode") > 0)
+end
